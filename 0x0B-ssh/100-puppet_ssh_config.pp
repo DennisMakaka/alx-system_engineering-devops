74915ab3c03dd279f2347cd749ed6faca4f5b417
Dennis Makaka <dennismakaka078@gmail.com>
2024-03-25 16:15:26 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,18 +1,40 @@
-#!/usr/bin/env bash
-# Using puppet to connect without password
+# Puppet manifest to modify the default SSH configuration file
+# enabling passwordless server connections by specifying a private key.
 
-file { '/etc/ssh/ssh_config':
-  ensure => present,
+include stdlib
+
+# Update SSH Private Key configuration
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/ssh_config',
-  line 	  => 'PasswordAuthentication no',
-  match	  => '^#PasswordAuthentication',
+# Explanation of Regex Match:
+# ^               - Start of the line
+# [#]*            - Any number of hash characters
+# [\s]*           - Zero or more white space characters
+# (?i)IdentityFile - Case-insensitive match for "IdentityFile"
+# [\s]+           - At least one whitespace character
+# ~/.ssh/id_rsa   - Path to the SSH private key file to replace
+# $               - End of the line
+
+# Update SSH Configuration to Deny Password Authentication
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true
 }
 
-file_line { 'Declare identity file':
-  path    => '/etc/ssh/ssh_config':
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#IdentityFile',
-}
+# Explanation of Regex Match:
+# ^                           - Start of the line
+# [#]*                        - Any number of hash characters
+# [\s]*                       - Zero or more white space characters
+# (?i)PasswordAuthentication   - Case-insensitive match for "PasswordAuthentication"
+# [\s]+                       - At least one whitespace character
+# (yes|no)                    - Match for either "yes" or "no"
+# $                           - End of the line
