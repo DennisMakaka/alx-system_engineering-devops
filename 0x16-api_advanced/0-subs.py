8f7c453b00e5752fce4eac13f2a3eb8e6a79e2e0
Dennis Makaka <dennismakaka078@gmail.com>
2024-08-15 10:25:14 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,35 +1,19 @@
-eddit Subscribers Query
+#!/usr/bin/python3
+"""
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
-def get_subscribers(subreddit):
-    """
-    Get the total number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The subreddit to query.
-    
-    Returns:
-        int: The total number of subscribers for the subreddit, or 0 if invalid.
-    """
+
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
-    
     response = requests.get(url, headers=headers, allow_redirects=False)
-    
     if response.status_code == 200:
         data = response.json()
-        subscribers = data.get("data", {}).get("subscribers", 0)
+        subscribers = data['data']['subscribers']
         return subscribers
     else:
         return 0
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please provide a subreddit as an argument.")
-    else:
-        subreddit = sys.argv[1]
-        print(get_subscribers(subreddit))
